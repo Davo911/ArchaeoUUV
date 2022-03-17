113562e5f4d33630191757d734c69a03097e0d84
Thomas-David Griedel <T_D_Griedel@gmx.de>
2022-03-17 01:50:59 +0100

    Create setup.sh

---
+++ BOOT/setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get install build-essential python3-dev python3-smbus python3-pip
+pip install numpy
+pip3 install numpy
+sudo pip3 install adafruit-circuitpython-ads1x15
+pip3 install Adafruit-Blinka
