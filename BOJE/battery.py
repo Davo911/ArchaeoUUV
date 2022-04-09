b66ca50fa22eb9752f561d827fdcc5906284e760
davo911 <thomas-david.griedel2@htw-dresden.de>
2022-04-09 17:22:41 +0200

    battery streamer

--- BOJE/battery.py
+++ BOJE/battery.py
@@ -2,9 +2,9 @@ from dronekit import connect
 import time, socket
 
 connection_boje = 'localhost:14550'
-#   Open socket to Boot-pi
-BOOT_IP = "192.168.2.2"
-BOOT_PORT = 27213
+#   Open socket to GCS
+BOOT_IP = "192.168.2.1"
+BOOT_PORT = 27123
 sock_boot = socket.socket(socket.AF_INET, # Internet
                     socket.SOCK_DGRAM) # UDP
 
@@ -16,4 +16,4 @@ print("Sending voltage to "+ BOOT_IP +":"+ str(BOOT_PORT))
 while(boje):
     time.sleep(1)    
     sock_boot.sendto(bytes(str(boje.battery.voltage), encoding='utf8'), (BOOT_IP, BOOT_PORT))
-    print(boje.battery.voltage)
+    print(boje.battery)
